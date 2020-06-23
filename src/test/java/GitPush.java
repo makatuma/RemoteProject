0611141b2cb6e03731b7988c46bcb9cb00e035aa
Azamat Duishonkulov <dkc.dov@gmail.com>
2020-06-22 19:54:31 -0500

    Created new project and pushed it

---
+++ src/test/java/GitPush.java
@@ -0,0 +1,8 @@
+public class GitPush {
+    public static void main(String[] args) {
+        System.out.println("My name is Azamat.");
+        System.out.println("What is your name?");
+        System.out.println("How old are you?");
+
+    }
+}
