1ecc9b086e075ac017e8ad517f1707cd0e4c2a67
Azamat Duishonkulov <dkc.dov@gmail.com>
2020-06-22 20:21:07 -0500

    outputs in gitGitCommit java class

--- src/test/java/gitGitCommit.java
+++ src/test/java/gitGitCommit.java
@@ -1,4 +1,9 @@
 public class gitGitCommit {
+    public static void main(String[] args) {
+        System.out.println("New Changes in Git Class");
+        System.out.println("Azamat Duishonkulovich");
+        System.out.println("Duishonkulovich Azamat Duishonkulov");
+        System.out.println("Zema, Zema, Zema, Zema");
 
-
+    }
 }
